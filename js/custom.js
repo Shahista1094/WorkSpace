825a98f94fd9488a2f09047852af78bf55f4d1e0
CXZneha1211 <anamika.kushwaha@troology.com>
2021-12-11 10:58:59 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+    $('#owl1').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+               items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+    $('#owl2').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveclass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+    $('#owl3').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveclass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+    $('#owl4').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveclass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: true,
+                loop: false
+            }
+        }
+    });
